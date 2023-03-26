5df5d91901075c60b60ab4acb1df1d133f73eecf
Marco Palomino <aquanautx37@gmail.com>
2023-03-26 18:50:41 -0400

    Add init.sql

--- backend/app.py
+++ backend/app.py
@@ -11,14 +11,13 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
-MYSQL_USER = "root"
+
 # MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+MYSQL_USER = "root"
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "kardashiandb"
-# MYSQL_USER = "root"
+MYSQL_DATABASE = "badmoviesdb"
+# Change to your password. Default: CinemaSearch
 MYSQL_USER_PASSWORD = "CinemaSearch"
-# MYSQL_PORT = 3306
-# MYSQL_DATABASE = "badmoviesdb"
 
 mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
 
@@ -32,8 +31,8 @@ CORS(app)
 # but if you decide to use SQLAlchemy ORM framework, 
 # there's a much better and cleaner way to do this
 def sql_search(movie):
-    query_sql = f"""SELECT id,title,descr FROM episodes WHERE LOWER( title ) LIKE '%%{movie.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
+    query_sql = f"""SELECT imdb_rating,title,description FROM movies WHERE LOWER( title ) LIKE '%%{movie.lower()}%%' limit 10"""
+    keys = ["imdb_rating","title","description"]
     data = mysql_engine.query_selector(query_sql)
     return json.dumps([dict(zip(keys,i)) for i in data])
 
