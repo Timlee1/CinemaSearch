5c5c8937ab4b09c7d2000598f80867edfc684847
Timlee1 <71849482+Timlee1@users.noreply.github.com>
2023-04-17 00:38:05 -0400

    Added images to database

--- webscraping/webscraper.py
+++ webscraping/webscraper.py
@@ -18,14 +18,19 @@ def webscraper(input_csv):
             #print(soup)
             content = soup.find('script', type = "application/ld+json")
             content = str(content)
-            ind_des = content.index('"description":') + len('"description":')
-            ind_next = content.index(',"review":') 
-            description_mov = content[ind_des+1:ind_next-1] #clamped string by 1 to remove quotations
+            #ind_des = content.index('"description":') + len('"description":')
+            #ind_des_next = content.index(',"review":') 
+            #description_mov = content[ind_des+1:ind_des_next-1] #clamped string by 1 to remove quotations
             #print(description_mov)
-            df.loc[ind, 'description'] = description_mov
+            #df.loc[ind, 'description'] = description_mov
+
+            ind_img = content.index('"image":') + len('"image":')
+            ind_img_next = content.index(',"description":') 
+            img_link = content[ind_img+1:ind_img_next-1]
+            #print(img_link)
+            df.loc[ind, 'image'] = img_link
             #print(df)
         except Exception as e:
             print(e)
-        #break
-    df.to_csv(dirname + '\\'+'best_bad_movies_with_descriptions.csv',)
-webscraper('best_bad_movies.csv')
+    df.to_csv(dirname + '\\'+'best_bad_movies_with_descriptions_images.csv',)
+webscraper('best_bad_movies_with_descriptions.csv')
